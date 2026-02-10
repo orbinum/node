e98b365ba8d0514649f17249c9e006feb3201889
nol4lej <nome.ahumada@outlook.com>
2026-02-10 11:19:21 -0300

    refactor: wrap WasmBuilder configuration in a block and enable poseidon-native-runtime feature

--- template/runtime/build.rs
+++ template/runtime/build.rs
@@ -1,8 +1,11 @@
 fn main() {
 	#[cfg(feature = "std")]
-	substrate_wasm_builder::WasmBuilder::new()
-		.with_current_project()
-		.export_heap_base()
-		.import_memory()
-		.build()
+	{
+		substrate_wasm_builder::WasmBuilder::new()
+			.with_current_project()
+			.export_heap_base()
+			.import_memory()
+			.enable_feature("poseidon-native-runtime")
+			.build();
+	}
 }
