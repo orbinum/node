c8d8efad47e66a1263ba758c42036fe185244a4e
nol4lej <nome.ahumada@outlook.com>
2026-06-12 20:00:43 -0400

    fix: /dns4/ in spec

--- docker/testnet/generate-testnet-spec.sh
+++ docker/testnet/generate-testnet-spec.sh
@@ -46,7 +46,7 @@ cat "$DEPLOY_DIR/testnet-spec-plain.json" | jq '
     "/dns/rpc-2.testnet.orbinum.io/tcp/30333/p2p/12D3KooWKiLEh2Z8XZYGejL5ZMR6rwEdZHPTpVDW3nsXfSW8paLt"
   ] |
   .telemetryEndpoints = [
-    ["/dns/telemetry.polkadot.io/tcp/443/x-parity-wss/%2Fsubmit%2F", 0]
+    ["/dns4/telemetry.polkadot.io/tcp/443/x-parity-wss/%2Fsubmit%2F", 0]
   ]
 ' > "$DEPLOY_DIR/testnet-spec-customized.json"
 echo -e "${GREEN}✓ Chain spec customized${NC}\n"
