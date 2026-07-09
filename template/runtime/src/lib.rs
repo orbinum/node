eb12cf2fd542b65621be60d0898de48ce880e2a3
nol4lej <nome.ahumada@outlook.com>
2026-07-09 15:59:24 -0400

    chore(runtime): bump spec_version to 5, transaction_version to 3

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -197,10 +197,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("orbinum"),
 	impl_name: Cow::Borrowed("orbinum"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 5,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 2,
+	transaction_version: 3,
 	system_version: 1,
 };
 
