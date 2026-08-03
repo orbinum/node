9cc17141086f683c2c03de25b920856ae54f02b4
nol4lej <nome.ahumada@outlook.com>
2026-08-03 17:29:36 -0400

    fix(specs): point testnet bootnodes at the p2p hostnames

--- scripts/generate-specs/testnet.sh
+++ scripts/generate-specs/testnet.sh
@@ -42,8 +42,8 @@ cat "$DEPLOY_DIR/testnet-spec-plain.json" | jq '
   .properties.tokenSymbol = "ORB" |
   .properties.tokenDecimals = 18 |
   .bootNodes = [
-    "/dns/rpc-1.testnet.orbinum.io/tcp/30333/p2p/12D3KooWBzqb1AFLQJd4NooU7q6dSsYBFL85A8RPsJDLesfxHvbW",
-    "/dns/rpc-2.testnet.orbinum.io/tcp/30333/p2p/12D3KooWKiLEh2Z8XZYGejL5ZMR6rwEdZHPTpVDW3nsXfSW8paLt"
+    "/dns/p2p-1.testnet.orbinum.io/tcp/30333/p2p/12D3KooWBzqb1AFLQJd4NooU7q6dSsYBFL85A8RPsJDLesfxHvbW",
+    "/dns/p2p-2.testnet.orbinum.io/tcp/30333/p2p/12D3KooWKiLEh2Z8XZYGejL5ZMR6rwEdZHPTpVDW3nsXfSW8paLt"
   ] |
   .telemetryEndpoints = [
     ["/dns4/telemetry.polkadot.io/tcp/443/x-parity-wss/%2Fsubmit%2F", 0]
