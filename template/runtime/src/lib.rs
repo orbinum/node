c12dba0b82bea2ffc27798698abb6b441144437f
nol4lej <nome.ahumada@outlook.com>
2026-09-02 12:26:59 -0400

    build(runtime): bump spec_version to 11 for the ISMP runtime upgrade

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -160,7 +160,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("orbinum"),
 	impl_name: Cow::Borrowed("orbinum"),
 	authoring_version: 1,
-	spec_version: 10,
+	spec_version: 11,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
